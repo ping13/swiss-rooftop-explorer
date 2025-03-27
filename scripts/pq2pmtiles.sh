891c57ae48510e47cea5f1258d5a97df732b385e
Stephan Heuel <mail@ping13.net>
2025-03-27 14:48:19 +0100

    feat: add missing buildings to pq2pmtiles

--- scripts/pq2pmtiles.sh
+++ scripts/pq2pmtiles.sh
@@ -56,6 +56,22 @@ COPY (
         ROW_NUMBER() OVER() AS id
     FROM read_parquet('$3')
 
+    UNION ALL
+
+    SELECT
+        'Feature' AS type,
+        st_asgeojson(ST_FlipCoordinates(ST_Transform(geometry, 'EPSG:2056', 'EPSG:4326'))) AS geometry,
+        json_object(
+            'roof_max', DACH_MAX,
+            'roof_min', DACH_MIN,
+            'year', ERSTELLUNG_JAHR::INT,
+            'type', 3,
+            'id', ROW_NUMBER() OVER(),
+            'objecttype', OBJEKTART::VARCHAR
+        ) AS properties,
+        ROW_NUMBER() OVER() AS id
+    FROM read_parquet('$4')
+
 ) TO STDOUT (FORMAT json)
 ; 
-" |  tippecanoe -o $4 --force -J $SCRIPT_DIR/roofs.filter.json -zg -l roof  --progress-interval=10 --no-feature-limit --no-tile-size-limit --no-clipping  # --visvalingam --no-simplification-of-shared-nodes  --no-line-simplification --use-source-polygon-winding --no-clipping 
+" |  tippecanoe -o $5 --force -J $SCRIPT_DIR/roofs.filter.json -zg -l roof  --progress-interval=10 --no-feature-limit --no-tile-size-limit --no-clipping  # --visvalingam --no-simplification-of-shared-nodes  --no-line-simplification --use-source-polygon-winding --no-clipping 
